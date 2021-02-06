b503dae9510fac239181c143698c561665fef30e
Mehmet ABACIOĞLU <mabacioglu@gmail.com>
2021-02-06 23:22:21 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal; // Ampülden Generate constructor dedik.
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            // iş kodları
+            return _productDal.GetAll();
+        }
+    }
+}
